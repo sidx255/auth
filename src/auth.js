1508ade24d4bff1b43de3867df28189ca1ed4bbd
sidx255 <siddharth190820@gmail.com>
2023-03-06 15:54:14 +0530

    feat: dockerize auth

--- src/auth.js
+++ src/auth.js
@@ -1,15 +1,43 @@
 
+const redis = require("redis");
+// const jwt = require("jsonwebtoken");
 const express = require("express");
-const app = express();
-app.use(express.json());
-
 const Router = require("./routes/authRouter");
 
+const app = express();
+app.use(express.json());
+require("dotenv").config();
+
 const port = 5501;
+
+// redis
+const config = {
+  socket: {
+    host: "docker.for.mac.localhost",
+    port: 6379,
+  },
+};
+global.redisClient = redis.createClient(config);
+// global.redisClient = redis.createClient();
+
+
+global.redisClient.on("error", (err) => {
+  console.log("Redis error: ", err);
+});
+
+global.redisClient
+  .connect()
+  .then(() => {
+    console.log("Redis connected");
+  })
+  .catch((err) => {
+    console.log("Redis error: ", err);
+  });
+
 
 app.use("/", Router);
 //app.get('/', (req, res) => res.send('This is the client!'));
 
 app.listen(port, () => {
-  console.log(`Client listening at http://localhost:${port}`);
+  console.log(`Auth listening at http://localhost:${port}`);
 });
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,6 @@
 const db = require("../models");
 const JWT = require("jsonwebtoken");
-const authUtils = require("../utils/authUtils");
-require("dotenv").config();
+const authUtils = require("../utils/authUtils"); 
 
 const addUser = async (data) => {
   const { username, password } = data;
@@ -12,10 +11,14 @@ const addUser = async (data) => {
 const verifyUser = async (data) => {
   const { username, password } = data;
   const user = await db.user.findOne({ where: { username } });
+
+
   if (user) {
     const passwordMatch = await authUtils.checkAuth(password, user.password);
     if (passwordMatch) {
       const token = JWT.sign({ username }, process.env.JWT_SECRET, { expiresIn: "1D" });
+      //if(authUtils.verifyJWT(token)) 
+      await global.redisClient.set(username, token);
       return { token: token, success: true };
     }
   }
@@ -24,12 +27,19 @@ const verifyUser = async (data) => {
 
 const verifyJWT = async (token) => {
   const decoded = JWT.verify(token, process.env.JWT_SECRET);
+  if(!decoded) return false;
   const user = await db.user.findOne({ where: { username: decoded.username } });
-  if (user) {
-    return true;
+  // if (user) {
+  //   return true;
+  // }
+  // return false;
+  const savedToken = await global.redisClient.get(user.username);
+  if (savedToken !== token) {
+    return false;
   }
-  return false;
+  return true;
 };
+  
 
 module.exports = {
   addUser,
